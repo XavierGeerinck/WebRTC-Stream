d010882138af59a0ec16b3d24c654782f5534e24
Xavier Geerinck <thebillkidy@hotmail.com>
2014-01-28 22:09:36 +0100

    More work

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,11 +1,12 @@
 var Socket = function () {
     this.servers = { "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] };
+    this.ready = false;
 };
 
 Socket.prototype.connect = function () {
     var self = this;
     
-    this.localPeerChannel = new webkitRTCPeerConnection(this.servers, {
+    this.localPeerConnection = new webkitRTCPeerConnection(this.servers, {
         optional: [
             {
                 RtpDataChannels: true
@@ -13,7 +14,9 @@ Socket.prototype.connect = function () {
         ]
     });
     
-    this.localPeerChannel.ondatachannel = function (event) {
+    console.log('Created Local Peer Channel');
+    
+    this.localPeerConnection.ondatachannel = function (event) {
         self.receiveChannel = event.channel;
         self.receiveChannel.onmessage = function (event) {
             console.log(event.data);    
@@ -21,11 +24,13 @@ Socket.prototype.connect = function () {
     }
     
     try {
-        this.sendChannel = this.localPeerChannel.createDataChannel('sendDataChannel', { reliable: false });
+        this.sendChannel = this.localPeerConnection.createDataChannel('sendDataChannel', { reliable: false });
+        console.log('Created Send Data Channel');
     } catch (e) {
         console.log('Exception when creating send channel: ' + e.message);    
     }
     
+    this.localPeerConnection.onicecandidate = this.gotLocalCandidate;
     this.sendChannel.onopen = this.handleSendChannelStateChange;
     this.sendChannel.onclose = this.handleSendChannelStateChange;
 };
@@ -35,10 +40,28 @@ Socket.prototype.disconnect = function () {
 };
 
 Socket.prototype.send = function (key, data) {
-    this.sendChannel.send(data);
+    console.log('Trying to send'); // TODO: Catch this, then timeout for 5s
+    if (this.ready) {
+        console.log('Send Msg');
+        this.sendChannel.send(data);
+    }
 };
 
 Socket.prototype.handleSendChannelStateChange = function () {
     var readyState = this.sendChannel.readyState;
     console.log('Send Channel State: ' + readyState);
+    
+    if (readyState == 'open') {
+        this.ready = true;    
+    } else {
+        this.ready = false;    
+    }
+};
+
+Socket.prototype.gotLocalCandidate = function () {
+    console.log('Remote ICE Callback');
+    if (event.candidate) {
+        this.localPeerConnection.addIceCandidate(event.candidate);
+        console.log('Local ICE candidate: \n' + event.candidate.candidate);
+    }
 };
